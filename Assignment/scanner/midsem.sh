cd126d96c545a5ed55d04ef3f6c1a26a6e19dbba
aryan02420 <aryan02420@gmail.com>
2022-03-29 17:46:18 +0000

    update midsem.sh

--- Assignment/scanner/midsem.sh
+++ Assignment/scanner/midsem.sh
@@ -12,8 +12,8 @@ echo "How many distinct token types are there in your lexer design?"
 cat scanner.py | grep -o -P "tokens = {[A-Z ,]*}" | grep -o -P "[A-Z]+" | uniq | wc -l
 
 echo "How many of these token types are encoded into an enumerated type or a number?"
-echo "..."
+echo "0"
 
 echo "How many of these token types are just the lexemes themselves?"
 # cat scanner.py | grep -o -P "IDENT\[\'.*\'\] = [A-Z]+" | tr '[:upper:]' '[:lower:]' | sed "s/[a-z]+\[\'[a-z]+\'\].*/lol/g"
-cat scanner.py | grep -oP "\] = [A-Z]+" | uniq -u | wc -l
+cat scanner.py | grep -o -P "\] = [A-Z]+" | uniq -u | wc -l
